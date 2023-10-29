a7864d430fb16672191198468917cb2bfd5c1aa3
abhishek <abhishek620sah@gmail.com>
2023-10-29 14:03:50 +0530

    adding about us

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
       <Footer />
     </div>
   );
